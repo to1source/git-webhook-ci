f44d34a97529e574040a46a141ac61ec9d1ab6da
Joel Chu <joelchu@foxmail.com>
2021-06-12 16:21:04 +0800

    just stub some test

---
+++ tests/github.test.ts
@@ -0,0 +1,6 @@
+// tests/github.test.ts
+
+import test from 'ava'
+
+
+test.todo(`Should able to use a github config to connect`)
---
+++ tests/gitlab.test.ts
@@ -0,0 +1,6 @@
+// tests/gitee.test.ts
+
+import test from 'ava'
+
+
+test.todo(`Should able to use a gitlab config to connect`)
