15dca5b12ede1ffbd30b327024925bfe2ed0a5fa
Joel Chu <joelckchu@gmail.com>
2021-06-17 21:57:38 +0800

    updating how to calculate the secret

--- tests/fixtures/fake-callback.ts
+++ tests/fixtures/fake-callback.ts
@@ -1,14 +1,9 @@
 // tests/fixtures/fake-callback.ts
 
-
-
-
-
-
-
+import { headers as giteeHeaders, payload as giteePayload } from './gitee'
 
 
 // this will create a fake callback to the webhook listener with specific payload
 export function getFakeCallback(provider: string, port: number): any {
-
+  
 }
