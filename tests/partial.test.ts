10a12689c6079e9444d4f5d7a4c8a8cb3fdd883b
Joel Chu <joelckchu@gmail.com>
2021-06-18 14:02:04 +0800

    completely change the createBareServer signature

--- tests/partial.test.ts
+++ tests/partial.test.ts
@@ -1,4 +1,13 @@
 // tests/partial.test.ts
-
+import test from 'ava'
+import request from 'supertest'
 // using this to do TBD-ish to develop the new features
-// and testing the code part by part 
+// and testing the code part by part
+
+import { createServer } from 'http'
+
+test(`Try out the supertest with fake headers`, t => {
+  request()
+
+
+})
