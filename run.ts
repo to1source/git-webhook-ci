e6e6a57634d304205a7d6c82f803fe16d36a876f
Joel Chu <joelckchu@gmail.com>
2021-06-24 21:27:49 +0800

    update the run.ts to accept secret using the env var

--- run.ts
+++ run.ts
@@ -6,6 +6,7 @@ import { gitWebhookCi } from './src/main'
 const config = {
   port: 4567,
   provider: 'github',
+  secret: process.env.SECRET, // so we only pass this when run it
   cmd: (args: []): void => {
     console.log("Got callback")
     console.log(args)
